16651bdab2b864f33d22417299d231d22d33dfa8
dadakyle <dadakyle09@gmail.com>
2023-09-10 07:48:58 -0700

    update: github file url added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Satur
 app.get('/', (req, res) => {
 const{slack_name, track} = req.query
 const date = new Date()
-    res.json({slack_name, current_day: days[date.getDay()],utc_time: date.toISOString(), track, github_file_url: '', github_repo_url: 'https://github.com/Dadakyle/HNG-Project_001', status: 200})
+    res.json({slack_name, current_day: days[date.getDay()],utc_time: date.toISOString(), track, github_file_url: 'https://github.com/Dadakyle/HNG-Project_001/blob/main/index.js', github_repo_url: 'https://github.com/Dadakyle/HNG-Project_001', status: 200})
 })
 
 
