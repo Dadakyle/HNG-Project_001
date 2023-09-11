85700b3c10db1adc69dbc31470a3fa78dc4831a7
dadakyle <dadakyle09@gmail.com>
2023-09-11 00:53:39 -0700

    chamge

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 
 const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
 const{slack_name, track} = req.query
 const date = new Date()
     res.json({slack_name, current_day: days[date.getDay()],utc_time: date.toISOString(), track, github_file_url: 'https://github.com/Dadakyle/HNG-Project_001/blob/main/index.js', github_repo_url: 'https://github.com/Dadakyle/HNG-Project_001', status: 200})
